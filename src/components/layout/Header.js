fb3e64705e869727629ea3e4c4228b57475d6ed5
hamed khodabakhsh <hamed.kh.2323@gmail.com>
2022-09-29 23:59:57 +0330

    final project

---
+++ src/components/layout/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import BookIcon from "@mui/icons-material/Book";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <AppBar position="sticky">
+      <Container maxWidth="lg">
+        <Toolbar>
+          <Typography component="h1" variant="h5" fontWeight="bold" flex={1}>
+            <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
+              وبلاگ
+            </Link>
+          </Typography>
+          <Link to="/" style={{ color: "#fff" }}>
+            <BookIcon />
+          </Link>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
